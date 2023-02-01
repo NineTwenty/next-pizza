280a777ac56ffc7897a018dde892b1fbad2cdcb0
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2023-02-01 14:39:08 +0400

    Refactor useMenuPositions to transform data

--- src/utils/apiHooks.ts
+++ src/utils/apiHooks.ts
@@ -1,5 +1,12 @@
+import type {
+  IngredientState,
+  MenuPositionState,
+  ProductState,
+  ToppingState,
+} from 'types/client';
 import type { RouterInputs } from 'utils/api';
 import { api } from 'utils/api';
+import { reduceToStateObject } from 'utils/common';
 
 export function useMenuPositions({
   category,
@@ -8,6 +15,32 @@ export function useMenuPositions({
     {
       category,
     },
-    { staleTime: Infinity }
+    {
+      staleTime: Infinity,
+      select: ({ ingredients, menuPositions, products, toppings }) => ({
+        menuPositions: menuPositions.reduce<MenuPositionState>(
+          reduceToStateObject,
+          {
+            ids: [],
+            entities: {},
+          } satisfies MenuPositionState
+        ),
+
+        ingredients: ingredients.reduce<IngredientState>(reduceToStateObject, {
+          ids: [],
+          entities: {},
+        } satisfies IngredientState),
+
+        products: products.reduce<ProductState>(reduceToStateObject, {
+          ids: [],
+          entities: {},
+        } satisfies ProductState),
+
+        toppings: toppings.reduce<ToppingState>(reduceToStateObject, {
+          ids: [],
+          entities: {},
+        } satisfies ToppingState),
+      }),
+    }
   );
 }
