a33c78a0ea47e477fd7fbb80ae7239be879d09c2
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2023-04-12 17:32:29 +0400

    Use Modal in MenuPositionModal

--- src/components/MenuPositionModal.tsx
+++ src/components/MenuPositionModal.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from 'react';
 import { useEffect, useRef, useState } from 'react';
 import { ChevronDown } from 'react-feather';
 import { AnimatePresence, motion, useScroll } from 'framer-motion';
+import { Modal } from 'components/Modal';
 
 type MenuPositionModalProps = {
   closeCallback: () => void;
@@ -31,70 +32,71 @@ export function MenuPositionModal({
   return (
     <AnimatePresence onExitComplete={closeCallback}>
       {inState && (
-        // eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events
-        <div
-          data-testid='menu_position_modal'
-          onClick={(event) => event.stopPropagation()}
-          className='fixed top-0 left-0 z-10 h-full w-full'
-        >
-          <motion.div // Fade background
-            transition={{
-              type: 'tween',
-              ease: [0, 0, 0.25, 1],
-              duration: 0.45,
-            }}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className='absolute h-full w-full bg-black/60'
-          />
-          <motion.section // Main modal
-            transition={{
-              type: 'tween',
-              ease: [0, 0, 0.25, 1],
-              duration: 0.45,
-            }}
-            initial={{ y: '100%' }}
-            animate={{ y: 0 }}
-            exit={{ y: '100%' }}
-            className='relative top-0 h-full overflow-y-scroll overscroll-y-contain'
+        <Modal>
+          <div
+            data-testid='menu_position_modal'
+            onClick={(event) => event.stopPropagation()}
+            className='fixed top-0 left-0 z-10 h-full w-full'
           >
-            <motion.div // Position image, fixed in background in mobile layout
-              style={{ opacity: scrollYProgress }}
-              className='fixed top-0 aspect-square w-full bg-white'
-            >
-              <div className='flex h-full w-full items-center justify-center rounded-full bg-orange-200 text-center'>
-                {`Photo${scrollYProgress.get()}`}
-              </div>
-            </motion.div>
-            <main
-              ref={containerRef}
-              className='flex h-[101vh] flex-col justify-between overflow-y-auto overscroll-y-contain bg-white pb-24'
-            >
-              <div className='z-10'>
-                <div className='h-[50vw] w-full' />
-                <div ref={targetRef} className='h-[50vw] w-full' />
-              </div>
-              <section className='bg-white/60 px-4 pt-4 backdrop-blur-xl'>
-                {renderMainContent}
-              </section>
-            </main>
-            <footer className='fixed -bottom-2 flex h-min w-full justify-center bg-white/60 px-4 py-3 pb-5 backdrop-blur'>
-              {renderFooterContent(() => {
-                setInState(false);
-              })}
-            </footer>
-            <button
-              type='button'
-              onClick={() => {
-                setInState(false);
+            <motion.div // Fade background
+              transition={{
+                type: 'tween',
+                ease: [0, 0, 0.25, 1],
+                duration: 0.45,
               }}
-              className='fixed top-0 z-10 m-4 rounded-full bg-white shadow-[rgba(0,0,0,0.12)_0px_0px_12px]'
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className='absolute h-full w-full bg-black/60'
+            />
+            <motion.section // Main modal
+              transition={{
+                type: 'tween',
+                ease: [0, 0, 0.25, 1],
+                duration: 0.45,
+              }}
+              initial={{ y: '100%' }}
+              animate={{ y: 0 }}
+              exit={{ y: '100%' }}
+              className='relative top-0 h-full'
             >
-              <ChevronDown className='h-12 w-12' />
-            </button>
-          </motion.section>
-        </div>
+              <motion.div // Position image, fixed in background in mobile layout
+                style={{ opacity: scrollYProgress }}
+                className='fixed top-0 aspect-square w-full bg-white'
+              >
+                <div className='flex h-full w-full items-center justify-center rounded-full bg-orange-200 text-center'>
+                  {`Photo${scrollYProgress.get()}`}
+                </div>
+              </motion.div>
+              <main
+                ref={containerRef}
+                className='flex h-full flex-col justify-between overflow-y-auto bg-white pb-24'
+              >
+                <div className='z-10'>
+                  <div className='h-[50vw] w-full' />
+                  <div ref={targetRef} className='h-[50vw] w-full' />
+                </div>
+                <section className='bg-white/60 px-4 pt-4 backdrop-blur-xl'>
+                  {renderMainContent}
+                </section>
+                <footer className='fixed bottom-0 flex h-min w-full justify-center bg-white/60 px-4 py-3 backdrop-blur'>
+                  {renderFooterContent(() => {
+                    setInState(false);
+                  })}
+                </footer>
+              </main>
+              <button
+                type='button'
+                onClick={() => {
+                  setInState(false);
+                }}
+                className='fixed top-0 z-10 m-4 rounded-full bg-white shadow-[rgba(0,0,0,0.12)_0px_0px_12px]'
+              >
+                <ChevronDown className='h-12 w-12' />
+              </button>
+            </motion.section>
+          </div>
+        </Modal>
       )}
     </AnimatePresence>
   );
