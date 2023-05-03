dd61d924f2516d16e55e5f758f66eb55447b6302
NineTwenty <64965260+NineTwenty@users.noreply.github.com>
2023-05-03 20:15:29 +0400

    Add Loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+import { Loader as LoaderIcon } from 'react-feather';
+
+export function Loader() {
+  return (
+    <LoaderIcon className='h-36 max-h-full w-36 max-w-full animate-spin stroke-orange-500' />
+  );
+}
